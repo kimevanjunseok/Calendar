50b17c925a7b1b2386cd46fb4acecadaa9e319f5
kimevanjunseok <jnunseok@gmail.com>
2019-09-30 19:58:49 +0900

    index.html .css .js

--- myapp/home/views.py
+++ myapp/home/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+
 def index(request):
+    # weeks = {   "Sun": 0,
+    #             "Mon": 1,
+    #             "Tus": 2,
+    #             "Wed": 3,
+    #             "Thu": 4,
+    #             "Fri": 5,
+    #             "Sat": 7
+    #         }
+    # info = datetime
+    # today = info.date.today()
+    # year, month, day = map(int, str(today).split('-'))
+    # week = weeks[info.date(year, month, 1).strftime("%A")[:3]]
+
+    # context = {
+    #     "year": year,
+    #     "month": month,
+    #     "day" : day,
+    #     "week": week,
+    # }
     return render(request, 'index.html', {})
---
+++ myapp/static/js/index.js
@@ -0,0 +1,121 @@
+
+// const today = new Date();
+// const start = new Date(today.getFullYear(), today.getMonth(), 1)
+// const week = ['일', '월', '화', '수', '목', '금', '토'];
+// document.getElementById("current").innerHTML = today.getFullYear() + "년" + " " + (today.getMonth()+1) + "월" + week[today.getDay()] + week[start.getDay()];
+
+
+$(document).ready(function() {
+    var currentDate = new Date();
+    function generateCalendar(d) {
+        function monthDays(month, year) {
+            var result = [];
+            var days = new Date(year, month, 0).getDate();
+            for (var i = 1; i <= days; i++) {
+                result.push(i);
+            }       
+            return result;
+        }   
+        Date.prototype.monthDays = function() {
+            var d = new Date(this.getFullYear(), this.getMonth() + 1, 0);
+            return d.getDate();
+        };
+        var details = {
+            // totalDays: monthDays(d.getMonth(), d.getFullYear()),
+            totalDays: d.monthDays(),
+            weekDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            months: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+        };
+        var start = new Date(d.getFullYear(), d.getMonth()).getDay();
+        var cal = [];
+        var day = 1;
+        for (var i = 0; i <= 6; i++) {
+            cal.push(['<tr>']);
+            for (var j = 0; j < 7; j++) {
+                if (i === 0) {
+                    cal[i].push('<div class="day-name">' + details.weekDays[j] + '</div>');
+                } else if (day > details.totalDays) {
+                    cal[i].push('<td>&nbsp;</td>');
+                } else {
+                    if (i === 1 && j < start) {
+                        cal[i].push('<td>&nbsp;</td>');
+                    } else {
+                        cal[i].push('<td class="day">' + day++ + '</td>');
+                    }
+                }
+            }
+            cal[i].push('</tr>');
+        }
+        cal = cal.reduce(function(a, b) {
+            return a.concat(b);
+        }, []).join('');
+        $('table').append(cal);
+        $('#months').text(details.months[d.getMonth()]);
+        $('#year').text(d.getFullYear());
+        $('td.day').mouseover(function() {
+            $(this).addClass('hover');
+        }).mouseout(function() {
+            $(this).removeClass('hover');
+        });
+    }
+
+    $('#left').click(function() {
+        $('table').text('');
+        if (currentDate.getMonth() === 0) {
+            currentDate = new Date(currentDate.getFullYear() - 1, 11);
+            generateCalendar(currentDate);
+        } else {
+            currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1)
+            generateCalendar(currentDate);
+        }
+    });
+    $('#right').click(function() {
+        $('table').html('<tr></tr>');
+        if (currentDate.getMonth() === 11) {
+            currentDate = new Date(currentDate.getFullYear() + 1, 0);
+            generateCalendar(currentDate);
+        } else {
+            currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1)
+            generateCalendar(currentDate);
+        }
+    });
+    generateCalendar(currentDate);
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+$(function () {
+    $('#view li:first-child a').tab('show')
+});
+
+$(function () {
+    $('[data-toggle="popover"]').popover().on('inserted.bs.popover')
+});
+
+$('.week, .daily-calendar').click(function() {
+    $('#registerSchedule').modal('show');
+});
+
+$(".event-consecutive, .event, .event-repeated").click(function(event) {
+    event.stopPropagation();
+});
+
+$(function () {
+    $('#datetimepicker1').datetimepicker({
+        format: 'L'
+    });
+    $('#datetimepicker3').datetimepicker({
+        format: 'L'
+    });
+});
+
+$(function () {
+    $('#datetimepicker2').datetimepicker({
+        format: 'LT'
+    });
+    $('#datetimepicker4').datetimepicker({
+        format: 'LT'
+    });
+});
