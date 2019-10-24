17ab576575d9af89102c243d58babd19935b1617
kimevanjunseok <jnunseok@gmail.com>
2019-10-24 11:34:56 +0900

    views.py 메서드 함수

--- myapp/home/views.py
+++ myapp/home/views.py
@@ -13,21 +13,12 @@ def create(request):
     if request.method == "POST":
         title = request.POST.get('title')
         content = request.POST.get('content')
-
-        start_day = request.POST.get('start_day')
-        start_day = start_day.replace("/", "-")
-
-        end_day = request.POST.get('end_day')
-        end_day = end_day.replace("/", "-")
-
-        start_time = request.POST.get('start_time')
-        if not start_time:
-            start_time = '하루종일'
         
-        end_time = request.POST.get('end_time')
-        if not end_time:
-            end_time = '하루종일'
-            
+        start_day = __convert_day_format(request.POST.get('start_day'))
+        end_day = __convert_day_format(request.POST.get('end_day'))
+        start_time = __convert_time_display(request.POST.get('start_time'))
+        end_time = __convert_time_display(request.POST.get('end_time'))
+        
         calendars = Calendar(title=title, content=content, start_day=start_day, end_day=end_day, start_time=start_time, end_time=end_time)
 
         calendars.save()
@@ -40,3 +31,9 @@ def calendar_list(request):
     calendar = Calendar.objects.all()
     serializer = CalendarSerializer(calendar, many=True)
     return Response(serializer.data)
+
+def __convert_day_format(day):
+    return day.replace("/", "-")
+
+def __convert_time_display(time):
+    return '하루종일' if not time else time
